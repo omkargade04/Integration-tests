7345f0c1f09317ce6c32e92d871a5b32a0efb180
Omkar Gade <g.omkar1110@gmail.com>
2025-01-05 00:49:55 +0530

    refactor: updated script

--- scripts/run-integration.sh
+++ scripts/run-integration.sh
@@ -1,27 +1,8 @@
-#!/bin/bash
-
-# Start Docker containers
 docker-compose up -d
-
-# Wait for the database to be ready
 echo '🟡 - Waiting for database to be ready...'
-./scripts/wait-for-it.sh "localhost:5432" --timeout=60 --strict -- echo '🟢 - Database is ready!'
-
-# Run Prisma migrations
-echo "Running Prisma migrations..."
-npx prisma migrate dev --name init || {
-  echo "❌ - Prisma migrations failed!";
-  docker-compose down;
-  exit 1;
-}
-
-# Run tests
-echo "Running tests..."
-npm run test || {
-  echo "❌ - Tests failed!";
-  docker-compose down;
-  exit 1;
-}
-
-# Shut down Docker containers
+./scripts/wait-for-it.sh "postgresql://postgres:mysecretpassword@localhost:5432/postgres" --timeout=30 --strict -- echo '🟢 - Database is ready!'
+npx prisma migrate dev --name init
+npm run test
 docker-compose down
+
+
